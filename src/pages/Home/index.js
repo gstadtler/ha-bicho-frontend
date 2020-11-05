2f36fc8900b6f77020de0308e372f177bc6a661c
gstadtler <ghstadtler@gmail.com>
2020-11-05 01:32:12 -0300

    Construção da home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+//import Slideshow from '../../components/Slideshow';
+import Navbar from '../../components/Navbar';
+
+import './styles.css';
+import logoImg from '../../imagens/logo.svg';
+import pets from '../../imagens/pets2.svg';
+
+
+function Home() {
+  return (
+    <div id="home-page">
+      <Navbar />
+      <div className="conteudo">
+
+        <img src={pets} alt="Ha-bicho" style={{height: 'max-content', width: '100vw'}} />
+        <img src={logoImg} alt="Ha-bicho" width={500} height={125} />
+
+        <main>
+          <h1>Mediamos doações com empatia, transparência e praticidade.</h1>
+        </main>
+      </div>
+    </div>
+  );
+}
+
+export default Home;

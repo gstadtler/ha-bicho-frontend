ba92f40ff13163bfd567ef6bfcb60a2d06c9bb4c
gstadtler <ghstadtler@gmail.com>
2020-11-09 22:45:18 -0300

    Ajustes na página de cadastro de usuário

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,5 +1,10 @@
 import React, { useState } from 'react';
 import Navbar from '../../components/Navbar';
+import { Form } from "@unform/web";
+import Input from "../../components/Input";
+import { FaFacebook } from 'react-icons/fa';
+
+import logotipoVerde from '../../imagens/logotipo-verde.svg';
 import './styles.css';
 
 import FacebookLogin from 'react-facebook-login';
@@ -10,6 +15,20 @@ function Register() {
 	//const [login, setLogin] = useState(false);
 	//const [data, setData] = useState({});
 	//const [picture, setPicture] = useState('');
+
+	function handleSubmit() {
+
+	}
+
+
+	function togglePassword() {
+		const inputPassword  = document.getElementById("password-input");
+		if (inputPassword.type === "password") {
+			inputPassword.type = "text";
+		} else {
+			inputPassword.type = "password";
+		}
+	}
 
 	const responseFacebook = (response) => {
 		console.log(response);
@@ -30,23 +49,52 @@ function Register() {
 		<>
 			<Navbar />
 			<div id="Register">
-				<div className="login-fb">
-					<FacebookLogin
-						appId="398864984824000"
-						fields="name,email,picture"
-						scope="public_profile,user_friends"
-						callback={responseFacebook}
-					/>
-				</div>
-				<div className="login-google">
-					<GoogleLogin
-						clientId="969966321777-hj4ej23le0r0gs8ga68k2l2516q88mqh.apps.googleusercontent.com"
-						buttonText="LOGIN WITH GOOGLE"
-						onSuccess={responseGoogle}
-						onFailure={responseGoogle}
-					/>
-				</div>
+				<header>
+					<h1>Cadastro</h1>
+					<img src={logotipoVerde} alt="Ha-bicho" width="175" height="100" />
+				</header>
 
+				<main>
+					<div className="login-fb">
+						<FacebookLogin
+							appId="398864984824000"
+							fields="name,email,picture"
+							scope="public_profile,user_friends"
+							callback={responseFacebook}
+							icon={<FaFacebook />}
+							textButton="Entrar com Facebook"
+							cssClass="facebook-button"
+						/>
+					</div>
+					<div className="login-google">
+						<GoogleLogin
+							clientId="969966321777-hj4ej23le0r0gs8ga68k2l2516q88mqh.apps.googleusercontent.com"
+							buttonText="Entrar com Google"
+							onSuccess={responseGoogle}
+							onFailure={responseGoogle}
+							className="google-button"
+						/>
+					</div>
+				</main>
+
+				<footer>
+					<Form onSubmit={handleSubmit}>
+						<Input name="username" placeholder="Nome de usuário" required />
+						<Input name="email" placeholder="Email" type="email" required />
+						<Input name="password" id="password-input" placeholder="Senha" type="password" required />
+						<div className="check-buttons">
+							<div className="toggle-password">
+								<input type="checkbox" onClick={togglePassword} className="check-input" />
+								<h6>Mostrar senha</h6>
+							</div>
+							<div className="check-terms">
+								<input type="checkbox" className="check-input" />
+								<h6>Concordo com os termos e condições</h6>
+							</div>
+						</div>
+						<button className="register-button" type="submit">Cadastrar</button>
+					</Form>
+				</footer>
 			</div>
 		</>
 	);
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,10 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import Abrigos from './pages/Abrigos';
 import Register from './pages/Register';
+import SobreNos from './pages/SobreNos';
+import Contato from './pages/Contato';
+import AbrigoPerfil from './pages/AbrigoPerfil';
+import Faq from './pages/Faq';
 
 function Routes() {
 	return (
@@ -11,6 +15,10 @@ function Routes() {
 				<Route path="/" exact component={Home} />
 				<Route path="/abrigos" component={Abrigos} />
 				<Route path="/register" component={Register} />
+				<Route path="/quem-somos" component={SobreNos} />
+				<Route path="/contato" component={Contato} />
+				<Route path="/perfil-abrigo/:abrigoId" component={AbrigoPerfil} />
+				<Route path="/faq" component={Faq} />
 			</Switch>
 		</BrowserRouter>
 	);

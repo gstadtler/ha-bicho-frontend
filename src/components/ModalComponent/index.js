00f431d41a32184268b9c30f90069fa0fe352021
gstadtler <ghstadtler@gmail.com>
2020-11-12 22:55:03 -0300

    Criando Modal com a lib reactstrap

---
+++ src/components/ModalComponent/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import {
+	Container,
+	Modal,
+	Button,
+} from 'reactstrap';
+
+import AbrigoForm from '../AbrigoForm';
+
+import { FiX } from "react-icons/fi";
+import './styles.css';
+
+function ModalComponent() {
+	const [showModal, setShowModal] = useState(false);
+
+	function handleOpenModal() {
+		setShowModal(!showModal);
+	}
+
+	function handleCloseModal() {
+		setShowModal(!showModal);
+	}
+
+	return (
+		<Container className="modal-form">
+			<Button className="bt-open-modal" onClick={handleOpenModal}>
+				Cadastre seu Abrigo
+			</Button>
+			<Modal 
+				isOpen={showModal} 
+				backdrop={true} 
+				keyboard={true}
+				className="modal-lg modal-component"
+			>
+				<Button className="bt-close-modal" onClick={handleCloseModal}>
+					<FiX />
+				</Button>
+				<AbrigoForm setModal={handleCloseModal} />
+			</Modal>
+		</Container>
+	);
+}
+
+export default ModalComponent;

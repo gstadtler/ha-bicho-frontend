50b2c9dcd3cb226beca01e12896b9156e9b4bc3e
gstadtler <ghstadtler@gmail.com>
2020-11-20 01:47:18 -0300

    Incorporando navbar direto nas rotas

---
+++ src/components/WithNavigation/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import UserNavbar from '../UserNavbar';
+import MenuNavbar from '../MenuNavbar';
+import HomeNavbar from '../HomeNavbar';
+
+import { isAuthenticated } from '../../services/auth';
+
+/**
+ * A functional React component as a wrapper for a page with navigation.
+ * Renders the page's navbar and div
+ */
+function WithNavigation(props) {
+  return (
+    <div>
+      {isAuthenticated() ? (
+        <UserNavbar />
+      ) : (
+          <HomeNavbar />
+        )}
+
+      <div>
+        {props.children}
+      </div>
+
+    </div>
+  )
+}
+
+export default WithNavigation;

891550b099e48b9230779e6ec2851a4b60081474
gstadtler <ghstadtler@gmail.com>
2020-11-22 21:53:27 -0300

    Novo component de Slideshow

--- src/components/Slideshow/index.js
+++ src/components/Slideshow/index.js
@@ -1,39 +1,82 @@
-import React from 'react';
-import { Slide } from 'react-slideshow-image';
+import React, { useState } from 'react';
+import {
+  Carousel,
+  CarouselItem,
+  CarouselControl,
+  CarouselIndicators,
+  CarouselCaption
+} from 'reactstrap';
+
 import './styles.css';
 
-import pets from '../../imagens/pets.svg';
-import pets2 from '../../imagens/pets2.svg';
-import pets3 from '../../imagens/pets3.svg';
+import pets02 from '../../imagens/pets02.jpg';
+import pets01 from '../../imagens/pets01.jpg';
+import pets0 from '../../imagens/pets0.jpg';
  
-const slideImages = [
-  pets,
-  pets2,
-  pets3
+const items = [
+  {
+    src: pets0,
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: pets01,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: pets02,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
 ];
- 
-function Slideshow() {
+
+const SlideShow = (props) => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const [animating, setAnimating] = useState(false);
+
+  const next = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === items.length - 1 ? 0 : activeIndex + 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const previous = () => {
+    if (animating) return;
+    const nextIndex = activeIndex === 0 ? items.length - 1 : activeIndex - 1;
+    setActiveIndex(nextIndex);
+  }
+
+  const goToIndex = (newIndex) => {
+    if (animating) return;
+    setActiveIndex(newIndex);
+  }
+
+  const slides = items.map((item) => {
     return (
-      <div className="slide-container">
-        <Slide>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
-              <span>Slide 1</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
-              <span>Slide 2</span>
-            </div>
-          </div>
-          <div className="each-slide">
-            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
-              <span>Slide 3</span>
-            </div>
-          </div>
-        </Slide>
-      </div>
-    )
+      <CarouselItem
+        onExiting={() => setAnimating(true)}
+        onExited={() => setAnimating(false)}
+        key={item.src}
+      >
+        <img src={item.src} alt={item.altText} />
+        <CarouselCaption />
+      </CarouselItem>
+    );
+  });
+
+  return (
+    <Carousel
+      activeIndex={activeIndex}
+      next={next}
+      previous={previous}
+    >
+      <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={goToIndex} />
+      {slides}
+      <CarouselControl direction="prev" directionText="Previous" onClickHandler={previous} />
+      <CarouselControl direction="next" directionText="Next" onClickHandler={next} />
+    </Carousel>
+  );
 }
 
-export default Slideshow;
+export default SlideShow;

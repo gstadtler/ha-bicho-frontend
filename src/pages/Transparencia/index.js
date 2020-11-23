f69fe09ccdb1246498c183cc99afdf4bf4e8f36b
gstadtler <ghstadtler@gmail.com>
2020-11-22 21:54:28 -0300

    Criação da página de transparência (gastos)

---
+++ src/pages/Transparencia/index.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import { Container, Table } from 'reactstrap';
+import './styles.css';
+
+function Transparencia() {
+  return (
+    <>
+      <h1 className="transparencia-title">Tabela de Gastos</h1>
+      <Container className="p-5" fluid={true}>
+        <Table bordered hover>
+          <thead style={{background: "#bdd3d3"}}>
+            <tr>
+              <th>#Ano: 2020#</th>
+              <th>Janeiro</th>
+              <th>Fevereiro</th>
+              <th>Março</th>
+              <th>Abril</th>
+              <th>Maio</th>
+              <th>Junho</th>
+              <th>Julho</th>
+              <th>Agosto</th>
+              <th>Setembro</th>
+              <th>Outubro</th>
+              <th>Novembro</th>
+              <th>Dezembro</th>
+            </tr>
+          </thead>
+          <tbody style={{background: "#bdd3d3"}}>
+            <tr>
+              <th scope="row">Ração</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Vacinas</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Medicamentos</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Exames</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Cirurgias</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Infraestrutura</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Detetização</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Água</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+            <tr>
+              <th scope="row">Energia</th>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+              <td></td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </>
+  );
+}
+
+export default Transparencia;

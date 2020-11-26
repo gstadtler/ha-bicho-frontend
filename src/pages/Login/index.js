5290b65feb02b821bc90a7d16ca7eae075a33a5a
gstadtler <ghstadtler@gmail.com>
2020-11-26 02:04:10 -0300

    Corrigindo erros de cadastro e login + nova estilização

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -6,10 +6,16 @@ import { FaFacebookF } from 'react-icons/fa';
 
 import {
   Container,
+  Row,
+  Col,
+  Card,
+  CardHeader,
+  CardBody,
+  CardText,
   Form,
   FormGroup,
   Input,
-  Button
+  Button,
 } from 'reactstrap';
 
 import logotipoVerde from '../../imagens/logotipo-verde.svg';
@@ -25,9 +31,9 @@ function Login(props) {
   const [password, setPassword] = useState('');
 
   //Sign In with social media
-	const [userData, setUserData] = useState('');
-	const [fbError, setFbError] = useState('');
-	const [googleError, setGoogleError] = useState('');
+  const [userData, setUserData] = useState('');
+  const [fbError, setFbError] = useState('');
+  const [googleError, setGoogleError] = useState('');
 
   async function handleSignIn(e) {
     e.preventDefault();
@@ -42,103 +48,110 @@ function Login(props) {
   }
 
   function responseFacebook(response) {
-		if (response) {
-			if (!response.status) {
-				setUserData(response);
-				console.log(userData);
-				login(response.accessToken);
-				props.history.push("/");
-			} else {
-				setFbError(response);
-				console.log(fbError);
-			}
-		} else {
-			alert("Houve algum erro ao cadastrar-se.");
-		}
-	}
+    if (response) {
+      if (!response.status) {
+        setUserData(response);
+        console.log(userData);
+        login(response.accessToken);
+        props.history.push("/");
+      } else {
+        setFbError(response);
+        console.log(fbError);
+      }
+    } else {
+      alert("Houve algum erro ao cadastrar-se.");
+    }
+  }
 
   function responseGoogle(response) {
-		if(response) {
-			if (!response.error) {
-				setUserData(response.profileObj);
-				console.log(userData);
-				login(response.accessToken);
-				props.history.push("/");
-			} else {
-				setGoogleError(response);
-				console.log(googleError);
-			}
-		} else {
-			alert("Houve algum erro ao cadastrar-se.");
-		}
-	}
+    if (response) {
+      if (!response.error) {
+        setUserData(response.profileObj);
+        console.log(userData);
+        login(response.accessToken);
+        props.history.push("/");
+      } else {
+        setGoogleError(response);
+        console.log(googleError);
+      }
+    } else {
+      alert("Houve algum erro ao cadastrar-se.");
+    }
+  }
 
   return (
     <>
-      <Container className="Login" fluid={true}>
-        <header>
-          <h1>Login</h1>
-          <img src={logotipoVerde} alt="Ha-bicho" width="175" height="100" />
-        </header>
+      <Container className="login" fluid={true}>
+        <h1 className="login-title">Login</h1>
+        <Row>
+          <Col className="d-flex justify-content-center content-wrapper">
+            <Card>
+              <CardHeader className="d-flex justify-content-center">
+                <img src={logotipoVerde} alt="Ha-bicho" width="175" height="100" />
+              </CardHeader>
 
-        <footer>
-          <Form onSubmit={handleSignIn}>
-            <FormGroup className="social-login">
-              <div className="login-fb">
-                <FacebookLogin
-                  appId="398864984824000"
-                  fields="name,email"
-                  scope="public_profile,user_friends"
-                  callback={responseFacebook}
-                  icon={<FaFacebookF />}
-                  textButton="Entrar com Facebook"
-                  cssClass="facebook-button"
-                />
-              </div>
-              <div className="login-google">
-                <GoogleLogin
-                  clientId="969966321777-hj4ej23le0r0gs8ga68k2l2516q88mqh.apps.googleusercontent.com"
-                  buttonText="Entrar com Google"
-                  onSuccess={responseGoogle}
-                  onFailure={responseGoogle}
-                  className="google-button"
-                />
-              </div>
-            </FormGroup>
+              <CardBody>
+                <Form onSubmit={handleSignIn}>
+                  <FormGroup className="social-login">
+                    <div>
+                      <FacebookLogin
+                        appId="398864984824000"
+                        fields="name,email"
+                        scope="public_profile,user_friends"
+                        callback={responseFacebook}
+                        icon={<FaFacebookF />}
+                        textButton="Entrar com Facebook"
+                        cssClass="facebook-button"
+                      />
+                    </div>
+                    <div>
+                      <GoogleLogin
+                        clientId="969966321777-hj4ej23le0r0gs8ga68k2l2516q88mqh.apps.googleusercontent.com"
+                        buttonText="Entrar com Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        className="google-button"
+                      />
+                    </div>
+                  </FormGroup>
 
-            <FormGroup>
-              <Input
-                name="email"
-                value={email}
-                onChange={e => setEmail(e.target.value)}
-                type="email"
-                placeholder="Email"
-                required
-              />
-            </FormGroup>
+                  <FormGroup>
+                    <Input
+                      name="email"
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                      type="email"
+                      placeholder="Email"
+                      required
+                    />
+                  </FormGroup>
 
-            <FormGroup>
-              <Input
-                name="password"
-                value={password}
-                onChange={e => setPassword(e.target.value)}
-                type="password"
-                placeholder="Senha"
-                required
-              />
-            </FormGroup>
+                  <FormGroup>
+                    <Input
+                      name="password"
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                      type="password"
+                      placeholder="Senha"
+                      required
+                    />
+                  </FormGroup>
 
-            <FormGroup>
-              <Button className="login-button" type="submit">Entrar</Button>
-            </FormGroup>
+                  <FormGroup>
+                    <Button className="login-button" type="submit">Entrar</Button>
+                  </FormGroup>
 
-            <FormGroup className="register-group">
-              <h6>Ainda não possui cadastro?</h6>
-              <Link to="/register" className="link-register">Cadastrar-se</Link>
-            </FormGroup>
+                  <FormGroup className="register-group">
+                    <CardText tag="h6">Ainda não possui cadastro?</CardText>
+                    <Link to="/register" className="link-register">Cadastrar-se</Link>
+                  </FormGroup>
+                </Form>
+              </CardBody>
 
-          </Form>
-        </footer>
+
+            </Card>
+          </Col>
+        </Row>
       </Container>
     </>
   );
--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -6,10 +6,16 @@ import { FaFacebookF } from 'react-icons/fa';
 
 import {
 	Container,
+	Row,
+	Col,
+	Card,
+	CardHeader,
+	CardBody,
+	CardText,
 	Form,
 	FormGroup,
 	Input,
-	Button
+	Button,
 } from 'reactstrap';
 
 import logotipoVerde from '../../imagens/logotipo-verde.svg';
@@ -34,24 +40,24 @@ function Register(props) {
 
 	async function handleSignUp(e) {
 		e.preventDefault();
-    try {
-      const novoUsuario = {
+		try {
+			const novoUsuario = {
 				username,
 				email,
 				password,
 				role
-      }
+			}
 			const response = await api.post("/register", novoUsuario);
-      if (response.status === 200) {
+			if (response.status === 200) {
 				alert("Agora você pode ajudar vários bichinhos de uma forma mais fácil e transparente!");
 				login(response.data.token);
 				props.history.push("/");
-      } else {
-        alert("Houve algum erro durante o cadastro :(");
-      }
-    } catch (err) {
-      console.log(err);
-    };
+			} else {
+				alert("Houve algum erro durante o cadastro :(");
+			}
+		} catch (err) {
+			console.log(err);
+		};
 
 	}
 
@@ -72,7 +78,7 @@ function Register(props) {
 	}
 
 	function responseGoogle(response) {
-		if(response) {
+		if (response) {
 			if (!response.error) {
 				setUserData(response.profileObj);
 				console.log(userData);
@@ -88,87 +94,98 @@ function Register(props) {
 	}
 
 	return (
-		<>
-			<Container className="Register" fluid={true}>
-				<header>
-					<h1>Cadastro</h1>
-					<img src={logotipoVerde} alt="Ha-bicho" width="175" height="100" />
-				</header>
+    <>
+      <Container className="register" fluid={true}>
+        <h1 className="register-title">Cadastro</h1>
+        <Row>
+          <Col className="d-flex justify-content-center content-wrapper">
+            <Card>
+              <CardHeader className="d-flex justify-content-center">
+                <img src={logotipoVerde} alt="Ha-bicho" width="175" height="100" />
+              </CardHeader>
 
-				<footer>
-					<Form onSubmit={handleSignUp}>
-						<FormGroup className="social-login">
-							<div className="login-fb">
-								<FacebookLogin
-									appId="398864984824000"
-									fields="name,email"
-									scope="public_profile,user_friends"
-									callback={responseFacebook}
-									icon={<FaFacebookF />}
-									textButton="Inscreva-se com Facebook"
-									cssClass="facebook-button"
-								/>
-							</div>
-							<div className="login-google">
-								<GoogleLogin
-									clientId="969966321777-hj4ej23le0r0gs8ga68k2l2516q88mqh.apps.googleusercontent.com"
-									buttonText="Inscreva-se com Google"
-									onSuccess={responseGoogle}
-									onFailure={responseGoogle}
-									className="google-button"
-								/>
-							</div>
-						</FormGroup>
-						<FormGroup>
-							<Input
-								name="username"
-								value={username}
-								onChange={e => setUsername(e.target.value)}
-								placeholder="Nome de usuário"
-								required
-							/>
-						</FormGroup>
+              <CardBody>
+                <Form onSubmit={handleSignUp}>
+                  <FormGroup className="social-login">
+                    <div>
+                      <FacebookLogin
+                        appId="398864984824000"
+                        fields="name,email"
+                        scope="public_profile,user_friends"
+                        callback={responseFacebook}
+                        icon={<FaFacebookF />}
+                        textButton="Cadastrar-se com Facebook"
+                        cssClass="facebook-button"
+                      />
+                    </div>
+                    <div>
+                      <GoogleLogin
+                        clientId="969966321777-hj4ej23le0r0gs8ga68k2l2516q88mqh.apps.googleusercontent.com"
+                        buttonText="Cadastrar-se com Google"
+                        onSuccess={responseGoogle}
+                        onFailure={responseGoogle}
+                        className="google-button"
+                      />
+                    </div>
+                  </FormGroup>
+                  <FormGroup>
+                    <Input
+                      name="username"
+                      value={username}
+                      onChange={e => setUsername(e.target.value)}
+                      placeholder="Nome de usuário"
+                      required
+                    />
+                  </FormGroup>
 
-						<FormGroup>
-							<Input
-								name="email"
-								value={email}
-								onChange={e => setEmail(e.target.value)}
-								type="email"
-								placeholder="Email"
-								required
-							/>
-						</FormGroup>
+                  <FormGroup>
+                    <Input
+                      name="email"
+                      value={email}
+                      onChange={e => setEmail(e.target.value)}
+                      type="email"
+                      placeholder="Email"
+                      required
+                    />
+                  </FormGroup>
 
-						<FormGroup>
-							<Input
-								name="password"
-								value={password}
-								onChange={e => setPassword(e.target.value)}
-								type="password"
-								placeholder="Senha"
-								required
-							/>
-						</FormGroup>
+                  <FormGroup>
+                    <Input
+                      name="password"
+                      value={password}
+                      onChange={e => setPassword(e.target.value)}
+                      type="password"
+                      placeholder="Senha"
+                      required
+                    />
+                  </FormGroup>
 
-						<FormGroup>
-							<Button className="register-button" type="submit">Cadastrar-se</Button>
-						</FormGroup>
+                  <FormGroup>
+                    <Button className="register-button" type="submit">Cadastrar-se</Button>
+                  </FormGroup>
 
-						<FormGroup className="termos-group">
-							<h6>Ao clicar em “Cadastrar-se”, você concorda com os termos e condições.</h6>
-						</FormGroup>
+									<FormGroup className="termos-group">
+										<CardText>
+											Ao cadastrar-se você concorda com os 
+											nossos <Link to="#" className="link-termos">termos </Link> 
+											e <Link to="#" className="link-termos">condições.</Link>
+										</CardText>
+									</FormGroup>
 
-						<FormGroup className="login-group">
-							<h6>Já possui cadastro?</h6>
-							<Link to="/login" className="link-login">Entrar</Link>
-						</FormGroup>
+                  <FormGroup className="login-group">
+                    <CardText tag="h6">Já possui cadastro?</CardText>
+                    <Link to="/login" className="link-login">Entrar</Link>
+                  </FormGroup>
+                </Form>
+              </CardBody>
 
-					</Form>
-				</footer>
-			</Container>
-		</>
-	);
+
+            </Card>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Register;

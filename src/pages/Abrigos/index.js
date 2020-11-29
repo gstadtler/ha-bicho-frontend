9cd3513d903c5fd83ad797bb3e7c532bc5f06ce8
gstadtler <ghstadtler@gmail.com>
2020-11-29 19:27:16 -0300

    Adicionando paginação na listagem de abrigos

--- src/pages/Abrigos/index.js
+++ src/pages/Abrigos/index.js
@@ -1,20 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { 
-	Card,
-	CardHeader,
-	CardBody,
-	CardFooter,
-	CardTitle,
-	CardText,
-} from 'reactstrap';
-
+import { Card, CardHeader, CardBody, CardFooter,
+	CardTitle, CardText, Pagination, PaginationItem, 
+	PaginationLink } from 'reactstrap';
 import './styles.css';
 import api from '../../services/api';
 
 function Abrigos(props) {
 
 	const [abrigos, setAbrigos] = useState([]);
+  const [currentPage, setCurrentPage] = useState(0);
+	const pageSize = 3;
+	const pagesCount = Math.ceil(abrigos.length / pageSize);
+	
+  function handleClick(e, index) {
+    e.preventDefault();
+    setCurrentPage(index);
+  }
 
 	useEffect(() => {
 		let mounted = true;
@@ -36,8 +38,8 @@ function Abrigos(props) {
 		<>
 			<h1 className="titulo-abrigos">Lista de Abrigos</h1>
 			<Card style={{border: "none"}}>
-				{abrigos.map(abrigo =>
-					<CardBody key={abrigo.id} className="abrigo">
+				{abrigos.slice(currentPage * pageSize,(currentPage + 1) * pageSize).map((abrigo, i) =>
+					<CardBody key={i} className="abrigo">
 						<CardHeader style={{background: "none", border: "none", padding: "20px"}}>
 							<CardTitle tag="h3">
 								{abrigo.nome}
@@ -52,6 +54,46 @@ function Abrigos(props) {
 					</CardBody>
 				)}
 			</Card>
+			
+			<div className="pagination-wrapper">
+        <Pagination className="d-flex justify-content-center">
+					<PaginationItem>
+						<PaginationLink
+              onClick={() => setCurrentPage(0)}
+							first
+							href="#"
+						/>
+					</PaginationItem>
+          <PaginationItem disabled={currentPage <= 0}>
+            <PaginationLink
+              onClick={e => handleClick(e, currentPage - 1)}
+              previous
+              href="#"
+            />
+          </PaginationItem>
+          {[...Array(pagesCount)].map((page, i) =>
+            <PaginationItem active={i === currentPage} key={i}>
+              <PaginationLink onClick={e => handleClick(e, i)} href="#">
+                {i + 1}
+              </PaginationLink>
+            </PaginationItem>
+          )}
+          <PaginationItem disabled={currentPage >= pagesCount - 1}>
+            <PaginationLink
+              onClick={e => handleClick(e, currentPage + 1)}
+              next
+              href="#"
+            />
+          </PaginationItem>
+					<PaginationItem>
+						<PaginationLink
+              onClick={() => setCurrentPage(pagesCount - 1)}
+							last
+							href="#"
+						/>
+					</PaginationItem>
+        </Pagination>
+      </div>
 		</>
 	);
 }

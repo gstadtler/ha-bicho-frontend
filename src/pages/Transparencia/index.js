ac65958e77207d2f9e5d571e3ddcf2db28f6b503
gstadtler <ghstadtler@gmail.com>
2020-12-01 02:56:46 -0300

    Editando a visualização da tabela de gastos

--- src/pages/Transparencia/index.js
+++ src/pages/Transparencia/index.js
@@ -1,168 +1,59 @@
 import React from 'react';
-import { Container, Table } from 'reactstrap';
+import NumberFormat from 'react-number-format';
+import { Container, Row, Col, Card, CardHeader, CardBody, CardText, CardFooter, CardDeck, CardGroup } from 'reactstrap';
+import { MdPets } from 'react-icons/md';
 import './styles.css';
 
 function Transparencia(props) {
+
+  const despesas = ['Ração', 'Vacinas', 'Medicamentos', 'Exames',
+    'Cirurgias', 'Infraestrutura', 'Detetização',
+    'Água', 'Energia'];
+
+  //const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril',
+  //  'Maio', 'Junho', 'Julho', 'Agosto',
+  //  'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+
   return (
     <>
       <h1 className="transparencia-title">Tabela de Gastos</h1>
-      <Container className="p-5" fluid={true}>
-        <Table bordered hover>
-          <thead style={{background: "#bdd3d3"}}>
-            <tr>
-              <th>#Ano: 2020#</th>
-              <th>Janeiro</th>
-              <th>Fevereiro</th>
-              <th>Março</th>
-              <th>Abril</th>
-              <th>Maio</th>
-              <th>Junho</th>
-              <th>Julho</th>
-              <th>Agosto</th>
-              <th>Setembro</th>
-              <th>Outubro</th>
-              <th>Novembro</th>
-              <th>Dezembro</th>
-            </tr>
-          </thead>
-          <tbody style={{background: "#bdd3d3"}}>
-            <tr>
-              <th scope="row">Ração</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Vacinas</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Medicamentos</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Exames</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Cirurgias</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Infraestrutura</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Detetização</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Água</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <th scope="row">Energia</th>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-          </tbody>
-        </Table>
+      <span style={{
+        fontWeight: "600",
+        fontSize: "18px",
+        color: "#333366",
+        marginLeft: "20px"
+      }}>
+        *As tabelas são atualizadas mensalmente pelos abrigos
+      </span>
+      <Container className="d-flex justify-content-center gastos" fluid={true}>
+        <CardDeck>
+          {despesas.map(despesa => (
+            <Card className="">
+              <CardHeader className="d-flex justify-content-center">
+                <MdPets size={20} style={{ color: "#669999" }} />
+              </CardHeader>
+              <CardBody className="card-despesa">
+                <CardText>
+                  {despesa}
+                </CardText>
+              </CardBody>
+              <CardFooter className="card-mes">
+                <CardText>
+                  <strong>Ex: Janeiro</strong>
+                </CardText>
+                <CardText>
+                  <NumberFormat
+                    value={0}
+                    displayType={'text'}
+                    thousandSeparator={true}
+                    prefix={'R$'}
+                    className="valor"
+                  />
+                </CardText>
+              </CardFooter>
+            </Card>
+          ))}
+        </CardDeck>
       </Container>
     </>
   );
